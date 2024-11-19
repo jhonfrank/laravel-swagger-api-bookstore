8587a1a15cb5de3cfb7e4a08c59102b444a3423e
Jhon Frank Alvarado Echigoyen <jhonfrank.ae@outlook.com>
2024-11-18 19:44:29 -0500

    feat: :construction: Add JsonResource for models

---
+++ app/Http/Resources/OrderDetailResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class OrderDetailResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'unit_price' => $this->unit_price,
+            'quantity' => $this->quantity,
+            'sub_total' => $this->sub_total,
+            'order_id' => $this->order_id,
+            'book_id' => $this->book_id,
+            'created_at' => $this->created_at->toDateTimeString(),
+            'updated_at' => $this->updated_at->toDateTimeString(),
+        ];
+    }
+}
---
+++ app/Http/Resources/OrderResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class OrderResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'number' => $this->number,
+            'total' => $this->total,
+            'user_id' => $this->user_id,
+            'created_at' => $this->created_at->toDateTimeString(),
+            'updated_at' => $this->updated_at->toDateTimeString(),
+            'details' => OrderDetailResource::collection($this->details),
+        ];
+    }
+}
