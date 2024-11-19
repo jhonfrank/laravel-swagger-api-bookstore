eb9c867cce9513c75f484159f9f91e908551df51
Jhon Frank Alvarado Echigoyen <jhonfrank.ae@outlook.com>
2024-11-18 19:48:35 -0500

    refactor: :construction: Delete unused methods

--- app/Models/OrderDetail.php
+++ app/Models/OrderDetail.php
@@ -8,16 +8,6 @@ class OrderDetail extends Model
 {
     protected $fillable = ['unit_price', 'quantity', 'sub_total', 'order_id', 'book_id'];
 
-    public function order()
-    {
-        return $this->belongsTo(Order::class);
-    }
-
-    public function book()
-    {
-        return $this->belongsTo(Book::class);
-    }
-
     protected function casts(): array
     {
         return [
